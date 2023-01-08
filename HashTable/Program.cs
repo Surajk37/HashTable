985c14f8951c13f1d8184514f3c52d97eb049667
Surajk37 <surajkhot9993@gmail.com>
2023-01-08 22:30:43 +0530

    Welcome Message added

---
+++ HashTable/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tWelcome To Hash Table Programs\n");
+            Console.ReadLine();
+        }
+    }
+}
